6d14b6f53af19c3cfc79571e17777bda09a7f241
Alkade <alexander.yay.wow@gmail.com>
2022-02-01 15:52:19 +0100

    Added new activity

---
+++ app/src/main/java/se/umu/cs/c19aky/thirty/PointCount.kt
@@ -0,0 +1,11 @@
+package se.umu.cs.c19aky.thirty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PointCount : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_point_count)
+    }
+}
