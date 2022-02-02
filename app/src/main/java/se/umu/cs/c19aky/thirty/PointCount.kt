55cbd9359d012df6fb506fca93f7441d47cf44f9
Alkade <alexander.yay.wow@gmail.com>
2022-02-02 01:56:10 +0100

    Created return function for when activity is done

--- app/src/main/java/se/umu/cs/c19aky/thirty/PointCount.kt
+++ app/src/main/java/se/umu/cs/c19aky/thirty/PointCount.kt
@@ -1,12 +1,25 @@
 package se.umu.cs.c19aky.thirty
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class PointCount : AppCompatActivity() {
+
+    private fun returnResult(pointSum: Int) {
+        val data = Intent()
+        data.putExtra(EXTRA_POINT_SUM, pointSum)
+        setResult(Activity.RESULT_OK, data)
+        finish()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_point_count)
+
+
+
     }
 
 
