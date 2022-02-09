87d3d6a6014fd9c3c18d0e5a7189057b0aa1242b
Alkade <alexander.yay.wow@gmail.com>
2022-02-09 04:51:58 +0100

    Trying to use the new Category data class. Something is going on with the saving

--- app/src/main/java/se/umu/cs/c19aky/thirty/PointCalculator.kt
+++ app/src/main/java/se/umu/cs/c19aky/thirty/PointCalculator.kt
@@ -13,28 +13,32 @@ private const val KEY_SELECTED = "se.umu.cs.c19aky.selected_category"
 * Class for calculating and keeping track of points in categories*/
 class PointCalculator(numberOfCategories: Int = 9) {
 
-    private var selectedCategory: String = ""
-    private var categories: MutableMap<String, Int> = mutableMapOf()
+    private var selectedCategory: Int = -1
+    private var categories: MutableList<Category> = mutableListOf()
 
     init {
         // Standard categories
-        categories["Low"] = -1
-        for (x in 4 until numberOfCategories + 4) {
-            categories[x.toString()] = -1
+        categories.add(Category("Low", -1))
+        for (x in 1 until numberOfCategories + 1) {
+            categories.add(Category((x+3).toString(), -1))
         }
     }
 
     // Store categories, for restoring when able to
     fun storeCategories(outState: Bundle) {
         val points: ArrayList<Int> = arrayListOf()
-        points.addAll(categories.values)
+        for (x in this.categories) {
+            points.add(x.points)
+        }
         outState.putIntegerArrayList(KEY_POINTS, points)
 
         val categories: ArrayList<String> = arrayListOf()
-        categories.addAll(this.categories.keys)
+        for (x in this.categories) {
+            categories.add(x.categoryName)
+        }
         outState.putStringArrayList(KEY_CATEGORIES, categories)
 
-        outState.putString(KEY_SELECTED, selectedCategory)
+        outState.putInt(KEY_SELECTED, selectedCategory)
     }
 
     // Restore points, categories and selected category
@@ -43,45 +47,50 @@ class PointCalculator(numberOfCategories: Int = 9) {
         val categories: ArrayList<String> = savedInstanceState.getStringArrayList(KEY_CATEGORIES) as ArrayList<String>
 
         for (x in 0 until categories.size) {
-            this.categories[categories[x]] = points[x]
+            this.categories.add(Category(categories[x], points[x]))
+            Log.d(TAG, "Added ${categories[x]} with ${points[x]}")
         }
-        selectedCategory = savedInstanceState.getString(KEY_SELECTED) as String
+        this.selectedCategory = savedInstanceState.getInt(KEY_SELECTED)
     }
 
     // Check if a category has been chosen, returns true it is
     fun checkIfCategoryIsChosen(): Boolean {
-        return selectedCategory != ""
+        return selectedCategory != -1
     }
 
     // Unselect category
     fun unselectCategory() {
-        selectedCategory = ""
+        selectedCategory = -1
     }
 
     // Select category, returns true if category could be selected
     fun selectCategory(category: String): Boolean {
-        if (categories[category] == -1) {
-            selectedCategory = category
-            return true
+        for (x in categories.indices) {
+            if (categories[x].categoryName == category) {
+                if (categories[x].points == -1) {
+                    selectedCategory = x
+                    return true
+                }
+            }
         }
         return false
     }
 
     fun checkIfValidSelection(values: ArrayList<Int>): Boolean {
-        return if (selectedCategory == "Low") {
+        return if (categories[selectedCategory].categoryName == "Low") {
             true
         } else {
-            values.sum() == selectedCategory.toInt()
+            values.sum() == categories[selectedCategory].categoryName.toInt()
         }
     }
 
     // Calculate points using current category
     fun calculatePoints(valuesLow: ArrayList<Int>, values: ArrayList<Int>): Int {
         val sum = values.sum()
-        return if (selectedCategory == "Low") {
+        return if (categories[selectedCategory].categoryName == "Low") {
             calculatePointsLow(valuesLow)
         } else {
-            if (sum == selectedCategory.toInt()) {
+            if (sum == categories[selectedCategory].categoryName.toInt()) {
                 sum
             } else {
                 -1
@@ -108,37 +117,30 @@ class PointCalculator(numberOfCategories: Int = 9) {
         }
     }
 
-    // Add points to some category
+    // Add points to currently selected category
     fun addPoints(points: Int) {
-        var current = categories[selectedCategory]
-        if (current != null) {
-            current += if (current == -1) {
-                points + 1
-            } else {
-                points
-            }
+        categories[selectedCategory].points += if (categories[selectedCategory].points == -1) {
+            points + 1
         } else {
-            current = points
+            points
         }
-        categories[selectedCategory] = current
-    }
-
-    // Get points in current category
-    fun getPoints(): Int? {
-        return categories[selectedCategory]
     }
 
     // Get the total amount of points from all categories
     fun getTotalPoints(): Int {
         var sum = 0
-        for (x in categories.values) {
-            sum += x
+        for (x in categories) {
+            sum += x.points
         }
         return sum
     }
 
     // Get a list of points, each entry is the amount of points in that category
     fun getAllPoints(): ArrayList<Int> {
-        return ArrayList(categories.values)
+        val points: ArrayList<Int> = ArrayList()
+        for (x in categories) {
+            points.add(x.points)
+        }
+        return points
     }
 }
